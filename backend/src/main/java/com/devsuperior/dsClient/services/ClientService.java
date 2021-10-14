fff7d16e8be82a4c7af9e849d90b24b68c9b8677
Felipe Pagani <feh.pagani@gmail.com>
2021-10-14 14:41:11 -0300

    findAllPage, findById

---
+++ backend/src/main/java/com/devsuperior/dsClient/services/ClientService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dsClient.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsClient.dto.ClientDTO;
+import com.devsuperior.dsClient.entities.Client;
+import com.devsuperior.dsClient.repositories.ClientRepository;
+import com.devsuperior.dsClient.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client client = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(client);
+		
+	}
+	
+	
+
+}
